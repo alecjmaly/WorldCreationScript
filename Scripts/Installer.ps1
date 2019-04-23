2f893ab5391ccd45d17ef06f6a382e88e0d6fd62
Alec Maly <alecjmaly@gmail.com>
2019-04-23 11:29:47 -0400

    Added installer.ps1 functionality to prompt for file closure if open while runing Installer.bat

--- Scripts/Installer.ps1
+++ Scripts/Installer.ps1
@@ -1,24 +1,50 @@
+function pause() {
+    Write-Host -NoNewLine 'Press any key to continue...';
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+}
+
+
+$root = "."
+
 # delete files - will redownlaod and clean old unused files
-Get-ChildItem -Path "." | ForEach-Object { 
-    If(!($_.Name -eq "Data" -and (Test-Path -Path ".\Data")) -and !($_.Name -eq "master.csv" -and (Test-Path -Path ".\master.csv"))) {
-        Remove-Item -Path ".\$($_.Name)" -Force -Recurse -Confirm:$false
+Get-ChildItem -Path $root | ForEach-Object { 
+    If(!($_.Name -eq "Data" -and (Test-Path -Path "$($root)\Data")) -and !($_.Name -eq "master.csv" -and (Test-Path -Path "$($root)\master.csv"))) {
+        do {
+            $file_deleted = $true
+
+            try {
+                cls
+                Write-Host "`n`nDeleting from from $($_.FullName)"
+                Remove-Item -Path $_.FullName -Force -Recurse -Confirm:$false -ErrorAction Stop
+            } catch {
+                If ($_ -like "Cannot remove*") {
+                    Write-Warning "Please close all files at and below this path (if a folder) and press ENTER to continue`n"
+                    $file_deleted = $false
+                    pause
+                }  
+            }        
+        } while(!$file_deleted)
     } 
 }
 
 # download package files
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $client = new-object System.Net.WebClient
-$client.DownloadFile("https://github.com/alecjmaly/WorldCreationScript/archive/master.zip",".\file.zip")
+$client.DownloadFile("https://github.com/alecjmaly/WorldCreationScript/archive/master.zip","$($root)\file.zip")
 
 # unzip
-Expand-Archive .\file.zip -DestinationPath .
+Expand-Archive "$($root)\file.zip" -DestinationPath $root
 
 
 # Extract and overwrite all files (Only pull Data Directory if it doesn't already exist)
 Get-ChildItem -Path .\WorldCreationScript-master | ForEach-Object { 
-        Move-Item -Path $_.FullName -Destination ".\$($_.Name)" -Force 
+    If(!($_.Name -eq "Data" -and (Test-Path -Path "$($root)\Data")) -and !($_.Name -eq "master.csv" -and (Test-Path -Path "$($root)\master.csv"))) {
+        Write-Host "Moving from $($_.FullName) $($root)\$($_.Name)"
+        Move-Item -Path $_.FullName -Destination "$($root)\$($_.Name)" -Force 
+    }
 }
 
 # cleanup 
-Remove-Item -Path ".\file.zip"
-Remove-Item -Path ".\WorldCreationScript-master" -Confirm:$false -Recurse -Force
+Remove-Item -Path "$($root)\file.zip"
+Remove-Item -Path "$($root)\WorldCreationScript-master" -Confirm:$false -Recurse -Force
+
